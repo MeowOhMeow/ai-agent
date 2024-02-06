b1ff0053d6608224ef2ae3aae10f3520a9f91313
MeowOhMeow <abc22681157@gmail.com>
2024-02-06 20:34:59 +0800

    undo to not over encapsulated

--- api.py
+++ api.py
@@ -9,23 +9,34 @@ sys.path.append("VITS")
 
 from GPT.OpenAI_API import OpenAI_API
 from VITS.TTS import TTS
-from transTTS import TransTTS
+version = "v1"
+if version == "v1":
+    from Translator.Translator_v1 import Translator
+elif version == "v2":
+    from Translator.Translator_v2 import Translator
 
 
 class API:
     def __init__(self) -> None:
         self.openai_api = OpenAI_API()
-        self.transTTS = TransTTS()
-        self.rate = self.transTTS.rate
+        self.translator = Translator()
+        self.tts = TTS()
+        self.rate = self.tts.rate
 
     def regenerate_response(self, **kwargs):
         response = self.openai_api.regenerate_response()
-        audio = self.transTTS(response, **kwargs)
+        translated = self.translator(response)
+        audio = self.tts(translated, **kwargs)
+        print(f"Response: {response}")
+        print(f"Translated: {translated}")
         return response, audio
 
     def __call__(self, text: str, **kwargs) -> np.ndarray:
         response = self.openai_api(text)
-        audio = self.transTTS(response, **kwargs)
+        translated = self.translator(response)
+        audio = self.tts(translated, **kwargs)
+        print(f"Response: {response}")
+        print(f"Translated: {translated}")
         return response, audio
 
 
